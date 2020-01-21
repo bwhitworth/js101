b50602bff0761c936b118f0435895ada9224174e
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-01-20 20:43:50 -0600

    created employee object

--- objects.js
+++ objects.js
@@ -22,3 +22,42 @@ ageCheck(24); //party!!!!!!!
 ageCheck(); //undefined results in the 'else'
 
 console.log(' ');
+
+const expense = {
+  dateCreated: '01/17/2020',
+  location: 'Baja Burrito',
+  cost: 12.57,
+  isGood: true
+};
+
+// dot notation
+console.log(expense.cost);
+
+// bracket notation
+console.log(expense['cost']);
+console.log('');
+
+const employee = {
+  id: "asdf1234",
+  startDate: '01/01/2020',
+  storeNum: 2345,
+  firstName: 'Zoe',
+  lastName: 'Ames',
+  industry: 'healthcare',
+  passedDrugScreen: true,
+  salary: 3000000000000,
+  collegeDegree: true,
+  position: 'Underwater basket weaver',
+  supervisor: 'asdf7890'
+};
+
+const statusMaker = (emp) => {
+  if (emp.firstName.startsWith('M')) { //.startsWith or emp.firstName[0] both work here
+    emp.status = 'VIP'
+  } else {
+    emp.status = 'peasant'
+  };
+  return emp;
+};
+
+console.log(statusMaker(employee));
